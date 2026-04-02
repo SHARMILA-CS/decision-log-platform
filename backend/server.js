fd68aeeddfcc3105395fcd47dd4a6dd446220d99
SHARMILA-CS <sharmilas.cs23@bitsathy.ac.in>
2026-04-02 11:08:40 +0530

    Fix CORS issue

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.use(express.json());
 
 // Routes
